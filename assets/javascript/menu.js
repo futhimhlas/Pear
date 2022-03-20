6fd39681e045dcf9d3c58ca935996e2a0895b2fc
futhimhlas <futhimhl2@gmail.com>
2022-03-20 11:06:53 +0000

    Mobile menu updated to more reflect apple site. Right logo still needs fixing

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,10 +1,21 @@
 let hmb = document.getElementById("menu-btn")
 let mobNav = document.getElementById("menu")
+let menuContainer = document.getElementById("menu-container")
+let menuClickCounter = 0
 
 function hamburgerMenuToggle() {
     hmb.classList.toggle('open')
     mobNav.classList.toggle('hidden')
     document.body.classList.toggle('no-scroll')
+    if (menuClickCounter % 2 == 0){
+        menuContainer.classList.add("mobile-backgroundColor");
+    }
+    else {
+        menuContainer.classList.remove("mobile-backgroundColor");
+    }
 }
 
 hmb.addEventListener('click', hamburgerMenuToggle) /* Event listenmer which triggers our function*/ 
+hmb.addEventListener('click', function(){
+    menuClickCounter += 1;
+})
